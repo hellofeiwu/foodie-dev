dd6b91d1c7c07511455c3ddbea0da359998f3a9c
hellofeiwu <hellofeiwu@gmail.com>
2021-01-13 22:00:00 +0800

    add mapper scan annotation and debug try catch

--- foodie-dev-api/src/main/java/com/imooc/Application.java
+++ foodie-dev-api/src/main/java/com/imooc/Application.java
@@ -2,10 +2,16 @@ package com.imooc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("com.imooc.mapper")
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        try {
+            SpringApplication.run(Application.class, args);
+        }catch (Throwable e) {
+            e.printStackTrace();
+        }
     }
 }
