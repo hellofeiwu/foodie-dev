52d6f55fde9d24f8444d07682e7bc2c6dad9a369
Wu <fwu31@ford.com>
2021-01-19 14:13:29 +0800

    define request method

--- foodie-dev-api/src/main/java/com/imooc/controller/HelloController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/HelloController.java
@@ -1,11 +1,11 @@
 package com.imooc.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello() {
         return "hello world~";
     }
