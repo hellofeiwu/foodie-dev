b0abb31eb8b863d69d0c14c8699898a21c227544
hellofeiwu <hellofeiwu@gmail.com>
2021-01-27 21:47:53 +0800

    add 获取商品子分类 api

--- foodie-dev-api/src/main/java/com/imooc/controller/IndexController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/IndexController.java
@@ -3,13 +3,16 @@ package com.imooc.controller;
 import com.imooc.enums.YesOrNo;
 import com.imooc.pojo.Carousel;
 import com.imooc.pojo.Category;
+import com.imooc.pojo.vo.CategoryVO;
 import com.imooc.service.CarouselService;
 import com.imooc.service.CategoryService;
 import com.imooc.utils.IMOOCJSONResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,6 +39,18 @@ public class IndexController {
     @GetMapping("/cats")
     public IMOOCJSONResult cats() {
         List<Category> result = categoryService.queryAllRootLevelCat();
+        return IMOOCJSONResult.ok(result);
+    }
+
+    @ApiOperation(value = "获取商品子分类", notes = "获取商品子分类", httpMethod = "GET")
+    @GetMapping("/subCats/{rootCatId}")
+    public IMOOCJSONResult subCats(
+            @ApiParam(name = "rootCatId", value = "一级分类 id", required = true)
+            @PathVariable Integer rootCatId) {
+        if(rootCatId == null) {
+            return IMOOCJSONResult.errorMsg("商品分类不存在");
+        }
+        List<CategoryVO> result = categoryService.querySubCatList(rootCatId);
         return IMOOCJSONResult.ok(result);
     }
 }
--- foodie-dev-service/src/main/java/com/imooc/service/impl/CategoryServiceImpl.java
+++ foodie-dev-service/src/main/java/com/imooc/service/impl/CategoryServiceImpl.java
@@ -2,10 +2,14 @@ package com.imooc.service.impl;
 
 import com.imooc.enums.CategoryType;
 import com.imooc.mapper.CategoryMapper;
+import com.imooc.mapper.CategoryMapperCustom;
 import com.imooc.pojo.Category;
+import com.imooc.pojo.vo.CategoryVO;
 import com.imooc.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 
 import java.util.List;
@@ -15,12 +19,22 @@ public class CategoryServiceImpl implements CategoryService {
     @Autowired
     private CategoryMapper categoryMapper;
 
+    @Autowired
+    private CategoryMapperCustom categoryMapperCustom;
+
+    @Transactional(propagation = Propagation.SUPPORTS)
     public List<Category> queryAllRootLevelCat() {
         Example example = new Example(Category.class);
         Example.Criteria criteria = example.createCriteria();
         criteria.andEqualTo("type", CategoryType.LEVEL1.type);
 
         List<Category> result = categoryMapper.selectByExample(example);
+        return result;
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public List<CategoryVO> querySubCatList(Integer rootCatId) {
+        List<CategoryVO> result = categoryMapperCustom.getSubCatList(rootCatId);
         return result;
     }
 }
