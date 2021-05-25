579fad2e67286a262800c6d0ac94f041cff26c32
hellofeiwu <hellofeiwu@gmail.com>
2021-05-25 23:21:37 +0800

    add 用户下单 api

---
+++ foodie-dev-api/src/main/java/com/imooc/controller/OrdersController.java
@@ -0,0 +1,28 @@
+package com.imooc.controller;
+
+import com.imooc.pojo.bo.SubmitOrderBO;
+import com.imooc.utils.IMOOCJSONResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(value = "订单相关", tags = {"订单相关的api接口"})
+@RestController
+@RequestMapping("orders")
+public class OrdersController {
+
+    @ApiOperation(value = "用户下单", notes = "用户下单", httpMethod = "POST")
+    @PostMapping("/create")
+    public IMOOCJSONResult create(@RequestBody SubmitOrderBO submitOrderBO) {
+        System.out.println(submitOrderBO.toString());
+
+        // 1. 创建订单
+        // 2. 创建订单以后，移除购物车中已结算（已提交）的商品
+        // 3. 向支付中心发送当前订单，用于保存支付中心的订单数据
+
+        return IMOOCJSONResult.ok();
+    }
+}
