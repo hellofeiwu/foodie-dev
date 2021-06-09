a9af97ec7172465bbfd20825468f4eddad63c8dc
Wu <fwu31@ford.com>
2021-06-09 18:06:21 +0800

    add 修改用户信息 api

--- foodie-dev-service/src/main/java/com/imooc/service/impl/center/CenterUserServiceImpl.java
+++ foodie-dev-service/src/main/java/com/imooc/service/impl/center/CenterUserServiceImpl.java
@@ -2,11 +2,15 @@ package com.imooc.service.impl.center;
 
 import com.imooc.mapper.UsersMapper;
 import com.imooc.pojo.Users;
+import com.imooc.pojo.bo.center.CenterUserBO;
 import com.imooc.service.center.CenterUserService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
 
 @Service
 public class CenterUserServiceImpl implements CenterUserService {
@@ -19,5 +23,16 @@ public class CenterUserServiceImpl implements CenterUserService {
         Users user = usersMapper.selectByPrimaryKey(userId);
         user.setPassword(null);
         return user;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public Users updateUserInfo(String userId, CenterUserBO centerUserBO) {
+        Users user = new Users();
+        BeanUtils.copyProperties(centerUserBO, user);
+        user.setId(userId);
+        user.setUpdatedTime(new Date());
+        usersMapper.updateByPrimaryKeySelective(user);
+        return queryUserInfo(userId);
     }
 }
