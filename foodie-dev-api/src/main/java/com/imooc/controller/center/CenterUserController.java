d0033d934c1b3d376327111e08075e45b822a919
Wu <fwu31@ford.com>
2021-06-10 14:08:32 +0800

    开启 hibernate 验证

--- foodie-dev-api/src/main/java/com/imooc/controller/center/CenterUserController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/center/CenterUserController.java
@@ -10,10 +10,16 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Api(value = "用户信息接口", tags = {"用户信息相关接口"})
 @RestController
@@ -27,10 +33,17 @@ public class CenterUserController {
     public IMOOCJSONResult userInfo(
             @ApiParam(name = "userId", value = "用户id", required = true)
             @RequestParam String userId,
-            @RequestBody CenterUserBO centerUserBO,
+            @RequestBody @Valid CenterUserBO centerUserBO,
+            BindingResult result,
             HttpServletRequest request,
             HttpServletResponse response
             ) {
+        // 判断BindingResult是否包含错误的验证信息，如果有，则直接return
+        if (result.hasErrors()) {
+            Map<String, String> errorMap =  getErrors(result);
+            return IMOOCJSONResult.errorMap(errorMap);
+        }
+
         Users user = centerUserService.updateUserInfo(userId, centerUserBO);
 
         user.setPassword(null);
@@ -46,5 +59,18 @@ public class CenterUserController {
         // TODO 后续要改，增加令牌token，会整合进redis，分布式会话
 
         return IMOOCJSONResult.ok();
+    }
+
+    private Map<String, String> getErrors(BindingResult result) {
+        Map<String, String> map = new HashMap<>();
+        List<FieldError> list = result.getFieldErrors();
+        for (FieldError error : list) {
+            // 发送验证错误所对应的某一个属性
+            String errorField = error.getField();
+            // 验证错误的信息
+            String errorMsg = error.getDefaultMessage();
+            map.put(errorField, errorMsg);
+        }
+        return map;
     }
 }
