18f68ccdeafd66ddad53e1c4d0511d481ed8e840
Wu <fwu31@ford.com>
2021-06-10 16:20:07 +0800

    add 上传文件 api

--- foodie-dev-api/src/main/java/com/imooc/controller/BaseController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/BaseController.java
@@ -2,6 +2,8 @@ package com.imooc.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+
 @RestController
 public class BaseController {
     public static final String FOODIE_SHOPCART = "shopcart";
@@ -14,4 +16,9 @@ public class BaseController {
     // 微信支付成功会通知 -> 支付中心接到通知后会通知 -> 本后台
     //                                        -> 回调通知的url
     String payReturnUrl = "http://localhost:8088/orders/notifyMerchantOrderPaid";
+
+    // 用户上传头像的位置
+    public static final String IMAGE_USER_FACE_LOCATION = "." + File.separator + "upload" +
+                                                            File.separator + "faces";
+    // public static final String IMAGE_USER_FACE_LOCATION = "./upload/faces";
 }
--- foodie-dev-api/src/main/java/com/imooc/controller/center/CenterUserController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/center/CenterUserController.java
@@ -1,5 +1,6 @@
 package com.imooc.controller.center;
 
+import com.imooc.controller.BaseController;
 import com.imooc.pojo.Users;
 import com.imooc.pojo.bo.center.CenterUserBO;
 import com.imooc.service.center.CenterUserService;
@@ -9,14 +10,21 @@ import com.imooc.utils.JsonUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,7 +32,7 @@ import java.util.Map;
 @Api(value = "用户信息接口", tags = {"用户信息相关接口"})
 @RestController
 @RequestMapping("userInfo")
-public class CenterUserController {
+public class CenterUserController extends BaseController {
     @Autowired
     private CenterUserService centerUserService;
 
@@ -57,6 +65,68 @@ public class CenterUserController {
         );
 
         // TODO 后续要改，增加令牌token，会整合进redis，分布式会话
+
+        return IMOOCJSONResult.ok();
+    }
+
+    @ApiOperation(value = "用户头像修改", notes = "用户头像修改", httpMethod = "POST")
+    @PostMapping("uploadFace")
+    public IMOOCJSONResult uploadFace(
+            @ApiParam(name = "userId", value = "用户id", required = true)
+            @RequestParam String userId,
+            @ApiParam(name = "file", value = "用户头像", required = true)
+            MultipartFile file
+    ) {
+        // 定义头像保存的地址
+        String fileSpace = IMAGE_USER_FACE_LOCATION;
+        // 在路径上为每一个用户创建一个以userId命名的文件夹，用于区分不同用户上传
+        String uploadPathPrefix = File.separator + userId;
+
+        if (file == null) {
+            return IMOOCJSONResult.errorMsg("文件不能为空！");
+        }
+        // 开始文件上传
+        FileOutputStream fileOutputStream = null;
+        try {
+            // 获得上传文件的文件名
+            String fileName = file.getOriginalFilename();
+            if (StringUtils.isNotBlank(fileName)) {
+                // 文件重命名 imooc-face.png -> ["imooc-face", "png"]
+                String fileNameArr[] = fileName.split("\\.");
+
+                // 获取文件的后缀名
+                String suffix = fileNameArr[fileNameArr.length -1];
+
+                // face-{userId}.png
+                // 文件名重组 覆盖式上传，增量式：额外拼接当前时间
+                String newFileName = "face-" + userId + "." + suffix;
+
+                // 上传的头像最终保存的位置
+                String finalFacePath = fileSpace + uploadPathPrefix + File.separator + newFileName;
+
+                File outFile = new File(finalFacePath);
+                if (outFile.getParentFile() != null) {
+                    // 创建文件夹
+                    outFile.getParentFile().mkdirs();
+                }
+
+                // 文件输出保存到目录
+                fileOutputStream = new FileOutputStream(outFile);
+                InputStream inputStream = file.getInputStream();
+                IOUtils.copy(inputStream, fileOutputStream);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (fileOutputStream != null) {
+                try {
+                    fileOutputStream.flush();
+                    fileOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
         return IMOOCJSONResult.ok();
     }
