e61a93cab9e51d803aecb7930f25e499c185334a
Wu <fwu31@ford.com>
2021-06-11 15:50:43 +0800

    serve 静态资源

--- foodie-dev-api/src/main/java/com/imooc/config/WebMvcConfig.java
+++ foodie-dev-api/src/main/java/com/imooc/config/WebMvcConfig.java
@@ -4,9 +4,22 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    // 实现静态资源的映射
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations(
+                        "file:/upload", // 映射本地静态资源
+                        "classpath:/META-INF/resources/" // 上面映射静态资源后，swagger之前默认的映射就没有了，所以在这里再添加一下
+                );
+    }
+
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
